45ef4ea448f6fb7a56676dd0a2633c4e0f4bb9cd
song-757 <hyoarang72@daum.net>
2025-09-21 21:34:15 +0900

    test fruit

---
+++ plugins/shell/test_fruit.sh
@@ -0,0 +1,8 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ] ; then
+        echo "You selected Apple"
+elif [ $FRUIT == ORANGE ] ; then
+        echo "You selected Orange"
+elif [ $FRUIT == GRAPE ] ; then
+        echo "You selected Graph"
+fi

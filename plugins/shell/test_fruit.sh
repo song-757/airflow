7a08ff45313db4c206db45bd934d8e1a5ca85c24
song-757 <hyoarang72@daum.net>
2025-09-21 22:43:30 +0900

    오류수정

--- plugins/shell/test_fruit.sh
+++ plugins/shell/test_fruit.sh
@@ -5,4 +5,6 @@ elif [ $FRUIT == ORANGE ] ; then
         echo "You selected Orange"
 elif [ $FRUIT == GRAPE ] ; then
         echo "You selected Graph"
+else
+        echo "You selected Other Fruit"
 fi
